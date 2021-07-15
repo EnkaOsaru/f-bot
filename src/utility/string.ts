d5e484e6f5c9e3c5b2d64114c1e639675b6044c0
Ryosuke Takahashi <osaru2002729@gmail.com>
2021-07-15 22:37:46 +0900

    Add string conversion utilities

--- src/utility/string.ts
+++ src/utility/string.ts
@@ -11,3 +11,58 @@ export function replace(source: string, pattern: string, replacement: string, li
 
     return source;
 }
+
+export function getMonospace(string: string) {
+    let result = '';
+
+    for (let i = 0; i < string.length; i++) {
+        let charCode = string.charCodeAt(i);
+
+        if (0x30 <= charCode && charCode <= 0x39) {
+            // Monospace zero is at 0x1d7f6
+            charCode = 0x1d7f6 + charCode - 0x30;
+        } else if (0x41 <= charCode && charCode <= 0x5a) {
+            // Monospace uppercase A is at 0x1d670
+            charCode = 0x1d670 + charCode - 0x41;
+        } else if (0x61 <= charCode && charCode <= 0x7a) {
+            // Monospace lowercase a is at 0x1d68a
+            charCode = 0x1d68a + charCode - 0x61;
+        }
+
+        result += String.fromCodePoint(charCode);
+    }
+
+    return result;
+}
+
+export function getRegionalIndicator(index: number): string | null;
+export function getRegionalIndicator(char: string): string | null;
+export function getRegionalIndicator(indexOrChar: number | string) {
+    let index: number;
+
+    if (typeof indexOrChar === 'number') {
+        index = indexOrChar;
+    } else {
+        index = indexOrChar.toUpperCase().charCodeAt(0) - 0x41;
+    }
+
+    if (index < 0 || 25 < index || !Number.isInteger(index)) {
+        return null;
+    }
+
+    // Regional indicator A is at 0x1f1e6
+    return String.fromCodePoint(0x1f1e6 + index);
+}
+
+export function getNumberEmoji(number: number) {
+    if (number < 0 || 10 < number || !Number.isInteger(number)) {
+        return null;
+    }
+
+    // 10 is the special case
+    if (number === 10) {
+        return String.fromCodePoint(0x1f51f);
+    }
+
+    return String.fromCodePoint(0x30 + number, 0xfe0f, 0x20e3);
+}
